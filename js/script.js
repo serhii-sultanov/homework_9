0692667f03e4b14f2bd201ac16e69b97b0430f9a
serhii-sultanov <sshsultanov@gmail.com>
2022-08-09 14:32:28 +0300

    added new variables and console.log(FullDateOfBirth)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const date = new Date("1993-09-26");
+
+let birthDate = date.getDate();
+let birthMonth = date.getMonth() + 1;
+let birthYear = date.getFullYear();
+
+if (birthMonth < 10) {
+  birthMonth = `0${birthMonth}`;
+}
+
+const FullDateOfBirth = `${birthDate}.${birthMonth}.${birthYear}`;
+
+console.log(FullDateOfBirth);
